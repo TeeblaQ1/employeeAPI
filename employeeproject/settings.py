5e4e0da83b3c56dd34ac675e25e0c619de73da4d
Oluwatosin Olasupo <oluwateeblaq@gmail.com>
2021-05-01 15:37:16 +0100

    Set settings.py to get secret key from environment and debug = False

--- employeeproject/settings.py
+++ employeeproject/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n3fqx2x=*(+6c%e0knd*n&hjq5!a31oa7hnz7p57zy03u8&7*u'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
